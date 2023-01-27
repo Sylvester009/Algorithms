e92e22b5609a50f2cce34c15f930377087980c32
Samuel Sylvester <sasy260902@gmail.com>
2023-01-27 10:12:39 +0100

    plusminus

---
+++ plusminus.js
@@ -0,0 +1,15 @@
+function plusMinus(arr) {
+    // Write your code here
+let pos = 0, neg = 0 , zero = 0 , length = arr.length
+    arr.forEach( n => {
+        if( n > 0 )
+            pos++
+        else if( n < 0 )
+            neg++
+        else
+            zero++
+    } )
+    console.log( (pos / length).toFixed(6) )
+    console.log( (neg / length).toFixed(6) )
+    console.log( (zero / length).toFixed(6) )
+}
